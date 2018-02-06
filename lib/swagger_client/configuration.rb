e780627b972caf6eb4256ce1bae557393f030dfd
The SMS Works <30385093+TheSMSWorks@users.noreply.github.com>
2018-02-06 10:34:40 +0000

    Updated base URI

--- lib/swagger_client/configuration.rb
+++ lib/swagger_client/configuration.rb
@@ -129,7 +129,7 @@ module SwaggerClient
 
     def initialize
       @scheme = 'https'
-      @host = 'api.thesmsworks.co.uk:8080'
+      @host = 'api.thesmsworks.co.uk'
       @base_path = '/v1'
       @api_key = {}
       @api_key_prefix = {}
